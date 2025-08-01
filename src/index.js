4092ad4c94b04d9c893d1af13954915cbf40732e
blessing1818 <ring5117@naver.com>
2025-08-01 15:46:14 +0900

    d

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 if (Math.random() < 0.5) {
-  window.location.href = "/src/index2.html";
+  window.location.href = "./src/index2.html";
 } else {
-  window.location.href = "/src/index3.html";
+  window.location.href = "./src/index3.html";
 }
