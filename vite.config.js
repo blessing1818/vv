b4d85177c9cc589acc90176db0c2e3e10b8c1bea
tnwls48 <byj05095@naver.com>
2025-08-01 15:21:08 +0900

    s

--- vite.config.js
+++ vite.config.js
@@ -9,11 +9,11 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        nested: resolve(__dirname, 'src/index2.html'),
-        nested: resolve(__dirname, 'src/index3.html'),
-        nested: resolve(__dirname, 'src/recommend-black.html'),
-        nested: resolve(__dirname, 'src/recommend.html'),
-        nested: resolve(__dirname, 'src/recommend2.html'),
+        nested: resolve(__dirname, "src/index2.html")
+        nested: resolve(__dirname, 'src/index3.html')
+        nested: resolve(__dirname, 'src/recommend-black.html')
+        nested: resolve(__dirname, 'src/recommend.html')
+        nested: resolve(__dirname, 'src/recommend2.html')
         nested: resolve(__dirname, 'src/whisky-black.html')
       },
     },
